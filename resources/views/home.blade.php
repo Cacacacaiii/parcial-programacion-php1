d0e15e49f342b949defcf4e69678546b0533bf2e
Cacacacaiii <LuquitaCrespi@gmail.com>
2023-07-05 18:23:08 -0300

    Probando

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Login</h1>
+<br>
+
+<input type="text" name="email" placeholder="Ingresa tu Email">
+<br>
+<input type="password" name="Pasword" placeholder="Ingresa tu Password">
+<br>
+<button type="submit">Enviar</button>
+<br>
+<br>
+
+<h1>¿no tienes Usuario?</h1>
+<br>
+
+
+<h1>Registrate</h1>
+<br>
+<input type="text" name="ingresa_nombre" placeholder="Ingresa tu nombre de usuario">
+<br>
+<input type="text" name="ingresa_email" placeholder="Ingresa tu email">
+<br>
+<input type="password" name="ingresa_password" placeholder="Ingresa tu password">
+<br>
+<button type="submit">Enviar</button>
+
+
+
+</body>
+</html>
