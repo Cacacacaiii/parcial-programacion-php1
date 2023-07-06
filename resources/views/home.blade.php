0225de7abd1979856b551770e2dbbcf5fd2f9a76
Cacacacaiii <LuquitaCrespi@gmail.com>
2023-07-06 02:44:36 -0300

    Cree las migraciones de Autores y Posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,25 +10,25 @@
 <h1>Login</h1>
 <br>
 
-<input type="text" name="email" placeholder="Ingresa tu Email">
+<input type="text" name="email" placeholder="Ingrese su Email">
 <br>
-<input type="password" name="Pasword" placeholder="Ingresa tu Password">
+<input type="password" name="Pasword" placeholder="Ingrese su Password">
 <br>
 <button type="submit">Enviar</button>
 <br>
 <br>
 
-<h1>¿no tienes Usuario?</h1>
+<h1>¿No tienes Usuario?</h1>
 <br>
 
 
 <h1>Registrate</h1>
 <br>
-<input type="text" name="ingresa_nombre" placeholder="Ingresa tu nombre de usuario">
+<input type="text" name="ingresa_nombre" placeholder="Ingrese su nombre completo">
 <br>
-<input type="text" name="ingresa_email" placeholder="Ingresa tu email">
+<input type="text" name="ingresa_email" placeholder="Ingrese su email">
 <br>
-<input type="password" name="ingresa_password" placeholder="Ingresa tu password">
+<input type="password" name="ingresa_password" placeholder="Ingrese su password">
 <br>
 <button type="submit">Enviar</button>
 
