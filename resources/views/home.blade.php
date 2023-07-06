529a0a1dce4408cbf46fe69fa03626dd9d10db8f
Ian Grotiuz <iangrotgz@gmail.com>
2023-07-06 20:48:06 -0300

    Se agregó el controlador de inicio de sesión, rutas, vistas y se modificó la migración. El controlador maneja las credenciales del formulario de inicio de sesión y redirige al usuario según el resultado de la autenticación.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,16 @@
 <h1>Login</h1>
 <br>
 
-<input type="text" name="email" placeholder="Ingrese su Email">
-<br>
-<input type="password" name="Pasword" placeholder="Ingrese su Password">
-<br>
-<button type="submit">Enviar</button>
-<br>
-<br>
+<form action="{{ route('login') }}" method="POST">
+    @csrf
+    <input type="text" name="email" placeholder="Ingrese su Email">
+    <br>
+    <input type="password" name="password" placeholder="Ingrese su Password">
+    <br>
+    <button type="submit">Enviar</button>
+    <br>
+    <br>
+</form>
 
 <h1>¿No tienes Usuario?</h1>
 <br>
@@ -31,7 +34,6 @@
 <input type="password" name="ingresa_password" placeholder="Ingrese su password">
 <br>
 <button type="submit">Enviar</button>
-
 
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login')->name('login.submit');
