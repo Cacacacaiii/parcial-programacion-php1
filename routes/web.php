7d40b759d3296d87c77716dc1985c6b6fdd77d07
IanGrotiuz <iangrotgz@gmail.com>
2023-07-07 14:48:06 -0300

    Cree una clave de encriptacion

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () { return view('home');});
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login')->name('login.submit');
 Route::post('/register', 'Auth\LoginController@register')->name('register');
-Route::get('/', [HomeController::class, 'showHome'])->name('home');
+Route::get('/', [LoginController::class, 'showHome'])->name('home');
