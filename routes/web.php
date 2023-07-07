98574c3fffe5cb521ce168abd2d232e7c50b3bd2
IanGrotiuz <iangrotgz@gmail.com>
2023-07-07 12:12:07 -0300

    Aregue una ruta a web.php para el register

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login')->name('login.submit');
+Route::post('/register', 'Auth\LoginController@register')->name('register');
+
